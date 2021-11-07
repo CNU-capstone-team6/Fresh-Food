cb90a2fd891cbbd2f8a07e633d1539716b65163d
castlesu <shsj4952@naver.com>
2021-11-07 16:31:08 +0900

    유통등록 화면에서 홈버튼 생성

--- freshfood/lib/registration/registmain/regist_page.dart
+++ freshfood/lib/registration/registmain/regist_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:freshfood/registration/registmain/components/body.dart';
+import 'package:freshfood/home_page.dart';
 
 
 class RegistPage extends StatelessWidget {
@@ -11,6 +12,17 @@ class RegistPage extends StatelessWidget {
       appBar: AppBar(
         title: Center(child: Text("유통과정 등록/수정" , textAlign: TextAlign.center,)),
         backgroundColor: Color(0xff6593F0),
+        automaticallyImplyLeading: false,
+        leading: IconButton (
+          icon: Icon(Icons.first_page),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+        ),
+
       ),
       body: Body(),
     );
