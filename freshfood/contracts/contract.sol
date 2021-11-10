82be32c7c2a4020a168b7e731207a248c25d391b
Junseo Park <wnstj2096@gmail.com>
2021-11-10 23:36:09 +0900

    Modify modifyFood function

--- freshfood/contracts/contract.sol
+++ freshfood/contracts/contract.sol
@@ -23,8 +23,25 @@ contract Storage {
         return number;
     }
 
+    function getFoodData(uint _number) internal view returns (string memory, string memory) {
+        string memory name = '';
+        string memory origin = '';
+        for(uint i = 0; i < foods.length; i++) {
+            if(foods[i].number == _number) {
+                name = foods[i].name;
+                origin = foods[i].origin;
+                break;
+            }
+        }
+
+        return (name, origin);
+    }
+
     // 특정 식자재의 유통과정 정보 추가
-    function modifyFood(uint _number, string calldata _name, string calldata _origin, string calldata _author) external {
+    function modifyFood(uint _number, string calldata _author) external {
+        string memory _name;
+        string memory _origin;
+        (_name, _origin) = getFoodData(_number);
         foods.push(Food(_number, _name, _origin, _author));
         chainLengthCount[_number]++;
     }
